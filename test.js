a45ad8d5a4896df80d759919988df6c67cab2c9d
Marius Lundgård <studio@mariuslundgard.com>
2017-04-25 00:39:25 +0200

    Add tests

--- test.js
+++ test.js
@@ -27,14 +27,30 @@ describe('segmented-property', () => {
       const obj1 = {foo: {bar: 1}}
       const obj2 = property.set(obj1, 'foo/bar', 2)
       assert.notEqual(obj1, obj2)
+      assert.notEqual(obj1.foo, obj2.foo)
       assert.notEqual(obj1.foo.bar, obj2.foo.bar)
     })
 
     it('should only create new objects for changed paths', () => {
-      const obj1 = {foo: {bar: 1, baz: 1}}
-      const obj2 = property.set(obj1, 'foo/bar', 2)
+      const obj1 = {foo: {bar: {value: 1}, baz: {value: 1}}}
+      const obj2 = property.set(obj1, 'foo/bar/value', 2)
+      assert.notEqual(obj1, obj2)
+      assert.notEqual(obj1.foo, obj2.foo)
       assert.notEqual(obj1.foo.bar, obj2.foo.bar)
       assert.equal(obj1.foo.baz, obj2.foo.baz)
+    })
+
+    it('should return new value if no key is provided', () => {
+      const obj1 = {}
+      const obj2 = {}
+      assert.equal(property.set(obj1, null, obj2), obj2)
+    })
+
+    it('should not create new object if nothing was changed', () => {
+      const obj1 = {foo: {bar: 1}}
+      const obj2 = property.set(obj1, 'foo/bar', 1)
+
+      assert.equal(obj1, obj2)
     })
   })
 })
