1ddf160b3c81f57b43dca92f84ac699b11105887
Marius Lundgård <studio@mariuslundgard.com>
2021-02-22 22:01:21 +0100

    chore: improve semantic-release configuration

--- release.config.js
+++ release.config.js
@@ -13,16 +13,11 @@ module.exports = {
       prerelease: true,
     },
   ],
-
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/git',
-      {
-        assets: ['dist/**/*.{js,css}', 'docs', 'package.json'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      },
-    ],
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/git',
   ],
 }
