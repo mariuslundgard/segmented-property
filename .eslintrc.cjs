80ae037a5eac600ae4976b5f2489574ab79da4b4
Marius Lundgård <studio@mariuslundgard.com>
2023-10-27 23:15:12 +0200

    build: upgrade package config

---
+++ .eslintrc.cjs
@@ -0,0 +1,50 @@
+/** @type import('eslint').Linter.Config */
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['import', 'simple-import-sort', 'prettier'],
+  rules: {
+    'no-console': 'error',
+    'no-shadow': 'error',
+    'no-warning-comments': [
+      'warn',
+      {
+        location: 'start',
+        terms: ['todo', 'fixme'],
+      },
+    ],
+    'quote-props': ['warn', 'consistent-as-needed'],
+    'strict': ['warn', 'global'],
+  },
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+      extends: [
+        'eslint:recommended',
+        'plugin:prettier/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+      plugins: ['import', '@typescript-eslint', 'simple-import-sort', 'prettier'],
+      rules: {
+        '@typescript-eslint/explicit-module-boundary-types': 'error',
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/member-delimiter-style': 'off',
+        '@typescript-eslint/no-empty-interface': 'off',
+      },
+    },
+  ],
+}
